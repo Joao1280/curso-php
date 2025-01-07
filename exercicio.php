ed33e5121396015e5065927ae78a28cd6dc48584
Joao1280 <joaovictorsantana144@gmail.com>
2025-01-07 17:43:24 -0300

    teste

--- exercicio.php
+++ exercicio.php
@@ -16,12 +16,14 @@
         <h2>Visualização do Exercícios</h2>
     </header>
     <nav class="navegacao">
-        <a href="#" class="verde">Sem Formatação</a>
+        <a href="<?=  "/{$_GET['dir']}/{$_GET['file']}.php"?>" class="verde">Sem Formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
-           
+           <?php 
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+           ?>
         </div>
     </main>
     <footer class="rodape">
