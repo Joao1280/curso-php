3cc9949aa9c32f837e0d853743a10b1e7be5c054
Joao1280 <joaovictorsantana144@gmail.com>
2025-01-08 09:24:43 -0300

    código para dar inicio ao php

--- exercicio.php
+++ exercicio.php
@@ -15,15 +15,16 @@
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercícios</h2>
     </header>
-        <nav class="navegacao">
-            <  <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem formatação</a>
-            <a href="index.php" class="vermelho">Voltar</a>
-        </nav>
+    <nav class="navegacao">
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
+            class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
     <main class="principal">
         <div class="conteudo">
-           <?php 
+            <?php 
                 include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
-           ?>
+            ?>
         </div>
     </main>
     <footer class="rodape">
